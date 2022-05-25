17acd238c733ac86cb5dd5a9f4e1745ae5e34782
kazhamiakin <kazhamiakin@gmail.com>
2022-05-25 21:53:29 +0200

    HTTP method fix

--- src/main/java/it/smartcommunitylab/playandgo/engine/controller/ExternalController.java
+++ src/main/java/it/smartcommunitylab/playandgo/engine/controller/ExternalController.java
@@ -10,7 +10,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -66,7 +65,7 @@ public class ExternalController extends PlayAndGoController {
 		return campaignManager.unsubscribePlayerByTerritory(nickname, campaign);
 	}
 	
-	@GetMapping("/api/ext/campaign/game/placing")
+	@PostMapping("/api/ext/campaign/game/placing")
 	public Map<String, Double> getCampaignPlacing(
 			@RequestParam String campaignId,
 			@RequestBody List<String> nicknames,
