26f8867d9a44b99de7db523da55d5d1c4e65152c
micnori <micnori@users.noreply.github.com>
2022-08-17 10:46:01 +0200

    create ge player at campaign reg

--- src/main/java/it/smartcommunitylab/playandgo/engine/campaign/school/SchoolCampaignSubscription.java
+++ src/main/java/it/smartcommunitylab/playandgo/engine/campaign/school/SchoolCampaignSubscription.java
@@ -9,11 +9,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import it.smartcommunitylab.playandgo.engine.ge.GamificationEngineManager;
 import it.smartcommunitylab.playandgo.engine.manager.survey.SurveyManager;
 import it.smartcommunitylab.playandgo.engine.manager.survey.SurveyRequest;
 import it.smartcommunitylab.playandgo.engine.model.Campaign;
 import it.smartcommunitylab.playandgo.engine.model.CampaignSubscription;
 import it.smartcommunitylab.playandgo.engine.model.Player;
+import it.smartcommunitylab.playandgo.engine.util.Utils;
 
 @Component
 public class SchoolCampaignSubscription {
@@ -22,9 +24,11 @@ public class SchoolCampaignSubscription {
 	@Autowired
 	SurveyManager surveyManager;
 	
+	@Autowired
+	GamificationEngineManager gamificationEngineManager;
+
 	public CampaignSubscription subscribeCampaign(Player player, Campaign campaign, 
 			Map<String, Object> campaignData) throws Exception {
-		//TODO check external API
 		CampaignSubscription sub = new CampaignSubscription();
 		sub.setPlayerId(player.getPlayerId());
 		sub.setCampaignId(campaign.getCampaignId());
@@ -40,6 +44,10 @@ public class SchoolCampaignSubscription {
 			SurveyRequest sr = campaign.getDefaultSurvey();
 			surveyManager.assignSurveyChallenges(campaign.getCampaignId(), Arrays.asList(player.getPlayerId()), sr);
 		}
+		//create player on GE
+		if(Utils.isNotEmpty(campaign.getGameId())) {
+			gamificationEngineManager.createPlayer(player.getPlayerId(), campaign.getGameId());
+		}		
 		return sub;
 	}
 }
