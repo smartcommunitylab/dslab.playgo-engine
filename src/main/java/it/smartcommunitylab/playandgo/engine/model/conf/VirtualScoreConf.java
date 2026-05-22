46e5dc81ace8f8cb6587d337cb88deea2d8d08e0
micnori <micnori@users.noreply.github.com>
2026-05-22 11:48:50 +0200

    Update VirtualScoreConf.java

--- src/main/java/it/smartcommunitylab/playandgo/engine/model/conf/VirtualScoreConf.java
+++ src/main/java/it/smartcommunitylab/playandgo/engine/model/conf/VirtualScoreConf.java
@@ -9,9 +9,9 @@ public class VirtualScoreConf {
     Map<String, String> limitCompanyDesc = new HashMap<>();
     List<LimitConf> limits = new ArrayList<>();
     String label;
-    int scoreDailyLimit = -1;
-    int scoreWeeklyLimit = -1;
-    int scoreMonthlyLimit = -1;
+    double scoreDailyLimit = -1.0;
+    double scoreWeeklyLimit = -1.0;
+    double scoreMonthlyLimit = -1.0;
     int trackDailyLimit = -1;
     int trackWeeklyLimit = -1;
     int trackMonthlyLimit = -1;
@@ -31,15 +31,15 @@ public class VirtualScoreConf {
         return label;
     }
 
-    public int getScoreDailyLimit() {
+    public double getScoreDailyLimit() {
         return scoreDailyLimit;
     }
 
-    public int getScoreWeeklyLimit() {
+    public double getScoreWeeklyLimit() {
         return scoreWeeklyLimit;
     }
 
-    public int getScoreMonthlyLimit() {
+    public double getScoreMonthlyLimit() {
         return scoreMonthlyLimit;
     }
 
@@ -79,15 +79,15 @@ public class VirtualScoreConf {
         this.label = label;
     }
 
-    public void setScoreDailyLimit(int scoreDailyLimit) {
+    public void setScoreDailyLimit(double scoreDailyLimit) {
         this.scoreDailyLimit = scoreDailyLimit;
     }
 
-    public void setScoreWeeklyLimit(int scoreWeeklyLimit) {
+    public void setScoreWeeklyLimit(double scoreWeeklyLimit) {
         this.scoreWeeklyLimit = scoreWeeklyLimit;
     }
 
-    public void setScoreMonthlyLimit(int scoreMonthlyLimit) {
+    public void setScoreMonthlyLimit(double scoreMonthlyLimit) {
         this.scoreMonthlyLimit = scoreMonthlyLimit;
     }
 
